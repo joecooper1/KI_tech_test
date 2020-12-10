69e5e9a7bb4602f078e93081c538798195ae7bf5
joecooper1 <joecooper2991@gmail.com>
2020-12-10 14:49:13 +0000

    Edit App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App">
-    <Header/>
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
