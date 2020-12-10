71817a19f82721afd7d67bb97f7c6aad52d0ec8c
joecooper1 <joecooper2991@gmail.com>
2020-12-10 15:49:25 +0000

    Add main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { MainView, ArticleList } from "../styles/Main";
+
+export default function Main(props) {
+  return (
+    <MainView>
+      <ArticleList></ArticleList>
+    </MainView>
+  );
+}
---
+++ src/styles/Main.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainView = styled.main``;
+
+export const ArticleList = styled.ul`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
